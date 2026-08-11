6d333e7fbea5df00e14d9b94bb3dc1cfd5264dbd
Eril TS Carvalho <erilandocarvalho@gmail.com>
2026-08-11 20:51:09 -0100

    fix: update package.json and vite.umd.config.ts to correct output file names for UMD build

--- vite.umd.config.ts
+++ vite.umd.config.ts
@@ -3,15 +3,18 @@ import { resolve } from 'node:path';
 
 export default defineConfig({
     publicDir: false,
+
     build: {
         outDir: 'dist',
         emptyOutDir: false,
+
+        minify: 'oxc',
 
         lib: {
             entry: resolve(__dirname, 'src/umd.ts'),
             name: 'Notificate',
             formats: ['umd'],
-            fileName: () => 'notificate.umd.js',
+            fileName: () => 'notificate.min.js',
         },
 
         rolldownOptions: {
